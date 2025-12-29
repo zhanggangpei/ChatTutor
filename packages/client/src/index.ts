cfaa0afe384a8a8c6caecd9585c5f51fe3cff473
Acbox <acbox0328@gmail.com>
2025-12-29 09:07:54 +0800

    chore: add client sdk

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(8002);
+export const app = new Elysia().get("/", () => "Hello Elysia").listen(8002);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
